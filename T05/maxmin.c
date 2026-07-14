b8fc4bf37862f1a916668ab8d108da6860a4ec81
AsilbekAsqarov17 <asilbek.asqarov.2023@gamil.com>
2026-07-14 20:26:56 +0500

    project 5 tasks

---
+++ T05/maxmin.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+
+int a, b, c;
+int max, min;
+printf("Enter 3 numbers: ");
+scanf("%d %d %d", &a, &b, &c);
+
+max = a;
+if(a < b){
+	if(b < c){
+	max = c;
+	}
+	else{
+	max = b;
+	}
+}
+else if(a < c){
+max = c;
+}
+printf("Max number is %d\n", max);
+
+min = b;
+if(a < b){
+	if(a < c){
+	min = a;
+	}
+	else{
+	min= c;
+	}
+}
+else if(c < b){
+min = c;
+}
+
+printf("Min number is %d\n", min);
+
+return 0;
+}
